c481c318a3aa87097eccfeb866c6966651b4e15b
Noel Tesfalidet <noetes@outlook.com>
2021-12-06 23:44:40 +0100

    part one and two added!

--- advone.java
+++ advone.java
@@ -2,17 +2,48 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
-
+//partone
 public class advone {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("input1.txt"));
+        System.out.println("1 or 2?");
+        Scanner ch = new Scanner(System.in);
+        switch (ch.nextInt()) {
+            case 1:
+                partone(in);
+                break;
+            case 2:
+            parttwo(in);
+            break;
+        }
+
+}
+
+public static void partone(Scanner in) {
+    int[] a = new int[2000];
+    int i = 0;
+    while (in.hasNextInt())
+        a[i++] = in.nextInt();
+    int svar = 0;
+    for (i = 1; i < 2000; i++) {
+        if (a[i] > a[i - 1])
+            svar++;
+    }
+    System.out.println(svar);
+    }
+    public static void parttwo(Scanner in){
         int[] a = new int[2000];
         int i = 0;
+        int[] b = new int[1998];
         while (in.hasNextInt())
             a[i++] = in.nextInt();
         int svar = 0;
-        for(i = 1; i < 2000; i++){
-            if(a[i] > a[i-1])
+
+        for (int temp = 0; temp < b.length; temp++)
+            b[temp] = a[temp] + a[temp+1] + a[temp+2];
+
+        for (i = 1; i < b.length; i++) {
+            if (b[i] > b[i - 1])
                 svar++;
         }
         System.out.println(svar);
--- advtwo.java
+++ advtwo.java
@@ -8,25 +8,66 @@ public class advtwo {
 
         public static void main(String[] args) throws FileNotFoundException {
             Scanner in = new Scanner(new File("input2.txt"));
-            int verti = 0;
-            int horiz = 0;
-            String[] b = new String[2];
-            String nextLine;
-            int i = 0;
-            while (i++ < 1000) {
-                nextLine = in.nextLine();
-                b = nextLine.split(" ");
-                if(b[0].equals("forward")){
-                    verti += Integer.parseInt(b[1]);
-                }
-                else if(b[0].equals("up")){
-                    horiz -= Integer.parseInt(b[1]);
-                }
-                else if(b[0].equals("down")){
-                    horiz += Integer.parseInt(b[1]);
-                }
+
+            System.out.println("1 or 2?");
+            Scanner ch = new Scanner(System.in);
+            switch (ch.nextInt()) {
+                case 1:
+                    partone(in);
+                    break;
+                case 2:
+                    parttwo(in);
+                    break;
             }
-            System.out.println(horiz + " " + verti);
-            System.out.println(horiz * verti);
         }
+
+
+    public static void partone(Scanner in) {
+                int verti = 0;
+                int horiz = 0;
+                String[] b = new String[2];
+                String nextLine;
+                int i = 0;
+                while (i++ < 1000) {
+                    nextLine = in.nextLine();
+                    b = nextLine.split(" ");
+                    if(b[0].equals("forward")){
+                        verti += Integer.parseInt(b[1]);
+                    }
+                    else if(b[0].equals("up")){
+                        horiz -= Integer.parseInt(b[1]);
+                    }
+                    else if(b[0].equals("down")){
+                        horiz += Integer.parseInt(b[1]);
+                    }
+                }
+        System.out.println(horiz);
+                System.out.println(horiz * verti);
+
+    }
+
+    public static void parttwo(Scanner in){
+        int verti = 0;
+        int aim = 0;
+        int depth = 0;
+        String[] b = new String[2];
+        String nextLine;
+        int test = 0;
+        int i = 0;
+        while (i++ < 1000) {
+            nextLine = in.nextLine();
+            b = nextLine.split(" ");
+            if(b[0].equals("forward")){
+                verti += Integer.parseInt(b[1]);
+                depth = (aim * Integer.parseInt(b[1])) + depth ;
+            }
+            else if(b[0].equals("up")){
+                aim -= Integer.parseInt(b[1]);
+            }
+            else if(b[0].equals("down")){
+                aim += Integer.parseInt(b[1]);
+            }
+        }
+        System.out.println(verti * depth);
+    }
 }
